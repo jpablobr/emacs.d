2e559f986df89aa002fe8f8e33f7c107f8842c69
jpablobr <xjpablobrx@gmail.com>
2010-08-28 13:15:54 -0600

    moving back ecb to jpablobr.el

--- jpablobr.el
+++ jpablobr.el
@@ -10,6 +10,15 @@
 (require 'grep-edit)
 
 ;;; ---------------------------------------------------------
+;;; - ECB
+;;;
+(add-to-list 'load-path (concat vendor-dir "/ecb"))
+(require 'ecb)
+(setq ecb-tip-of-the-day nil)
+(setq ecb-primary-secondary-mouse-buttons (quote mouse-1--mouse-2))
+(ecb-activate)
+
+;;; ---------------------------------------------------------
 ;;; - Textmate
 ;;;
 (require 'textmate)
@@ -36,24 +45,23 @@
       '((".*\\.freenode.net" "#github" "#greasemonkey" "#emacs" "#heroku" "#sinatra")))
 
 ;;; ---------------------------------------------------------
-;;; - cheat
+;;; - Cheat
 ;;;
 (add-to-list 'auto-mode-alist '("\\.cheat$" . cheat))
-(require 'cheat)
 
 ;;; ---------------------------------------------------------
-;;; - redo
+;;; -Redo
 ;;;
 (require 'redo)
 (global-set-key [(control -)] 'redo)
 
 ;;; ---------------------------------------------------------
-;;; - find-recursive
+;;; - Find-recursive
 ;;;
 (require 'find-recursive)
 
 ;;; ---------------------------------------------------------
-;;; - yaml
+;;; - Yaml
 ;;;
 (add-to-list 'load-path (concat vendor-dir "/yaml-mode"))
 (require 'yaml-mode)
--- jpablobr/cedet.el
+++ jpablobr/cedet.el
@@ -3,13 +3,4 @@
 ;;; - CEDET
 (load-file "~/.emacs.d/vendor/cedet/common/cedet.el")
 
-;;; ---------------------------------------------------------
-;;; - ECB
-;;;
-(add-to-list 'load-path (concat vendor-dir "/ecb"))
-(require 'ecb)
-(setq ecb-tip-of-the-day nil)
-(setq ecb-primary-secondary-mouse-buttons (quote mouse-1--mouse-2))
-(ecb-activate)
-
 (provide 'cedet)
