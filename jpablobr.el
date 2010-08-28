b080089e6e2713855cd509848e88bdbf55245e78
jpablobr <xjpablobrx@gmail.com>
2010-08-28 09:53:40 -0600

    cleaning up jpablobr.el

--- jpablobr.el
+++ jpablobr.el
@@ -1,47 +1,69 @@
-;; Jose Pablo Barrantes emacs.d
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; -*- coding: utf-8-unix; -*-
+;;; ---------------------------------------------------------
+;;; - Jose Pablo Barrantes
+;;; - jpablobr's .emacs.d
+;;; - http://jpablobr.com
+;;;
 
-;; Textmate.el
+;;; ---------------------------------------------------------
+;;; - Textmate
+;;;
 (add-to-list 'load-path (concat vendor-dir "/textmate.el"))
 (require 'textmate)
 (textmate-mode)
 
-;; ecb
+;;; ---------------------------------------------------------
+;;; - ECB
+;;;
 (add-to-list 'load-path (concat vendor-dir "/ecb"))
 (require 'ecb)
 (setq ecb-tip-of-the-day nil)
 (setq ecb-primary-secondary-mouse-buttons (quote mouse-1--mouse-2))
 (ecb-activate)
 
-;; Artist-mode
+;;; ---------------------------------------------------------
+;;; - Artist-mode
+;;;
 (autoload 'artist-mode "artist" "Enter artist-mode" t)
 
-;; Twittering Mode
+;;; ---------------------------------------------------------
+;;; - Twittering Mode
+;;;
 (add-to-list 'load-path (concat vendor-dir "/twittering-mode"))
 (require 'twittering-mode)
 (setq twittering-username "jpablobr")
 
-;; IRC
+;;; ---------------------------------------------------------
+;;; - IRC
+;;;
 (require 'erc)
 (erc-autojoin-mode t)
 (setq erc-autojoin-channels-alist
       '((".*\\.freenode.net" "#github" "#greasemonkey" "#emacs" "#heroku" "#sinatra")))
 
-;; cheat
+;;; ---------------------------------------------------------
+;;; - cheat
+;;;
 (add-to-list 'load-path (concat vendor-dir "/cheat"))
 (add-to-list 'auto-mode-alist '("\\.cheat$" . cheat))
 (require 'cheat)
 
-;; redo
+;;; ---------------------------------------------------------
+;;; - redo
+;;;
 (add-to-list 'load-path (concat vendor-dir "/redo"))
-
 (require 'redo)
 (global-set-key [(control -)] 'redo)
-;; find-recursive
+
+;;; ---------------------------------------------------------
+;;; - find-recursive
+;;;
 (add-to-list 'load-path (concat vendor-dir "/find-recursive"))
 (require 'find-recursive)
 
-;; css
+;;; ---------------------------------------------------------
+;;; - css
+;;;
 (add-to-list 'load-path (concat vendor-dir "/css-mode"))
 (require 'css-mode)
 (autoload 'css-mode "css-mode" "Mode for editing CSS files" t)
@@ -50,7 +72,9 @@
           (lambda()
             (local-set-key (kbd "<return>") 'newline-and-indent)))
 
-;; yaml
+;;; ---------------------------------------------------------
+;;; - yaml
+;;;
 (add-to-list 'load-path (concat vendor-dir "/yaml-mode"))
 (require 'yaml-mode)
 (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
