363c28ef61dbae7de96c9aa43fa2bc2fe093dbb6
jpablobr <xjpablobrx@gmail.com>
2010-08-28 19:42:30 -0600

    modified install.rb

--- install.rb
+++ install.rb
@@ -3,4 +3,4 @@
 `cd vendor && git clone git@github.com:jpablobr/yasnippets-jpablobr.git yasnippets-jpablobr && git add yasnippets-jpablobr/ && cd yasnippets-jpablobr && git pull`
 puts `ls -la vendor`
 # Remiders
-puts "Don't forget to M-x byte-compile-file js2 and to add html-mode sass-mode and haml-mode to the auto-complete.el (line # 163)"
+puts "Don't forget to M-x byte-compile-file js2"
