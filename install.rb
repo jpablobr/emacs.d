a45bb8d51c213614d41c13dd1eef5c06214cbb3e
jpablobr <xjpablobrx@gmail.com>
2010-08-29 22:02:44 -0600

    submodules updates

--- install.rb
+++ install.rb
@@ -7,7 +7,7 @@ system %Q{ git submodule update --init }
 
 # Fake submoduling yasnippets-jpablobr
 system %Q{ cd vendor && git clone git@github.com:jpablobr/yasnippets-jpablobr.git yasnippets-jpablobr }
-system %Q{ git add yasnippets-jpablobr/ && cd yasnippets-jpablobr && git pull }
+system %Q{ cd vendor && git add yasnippets-jpablobr/ && cd yasnippets-jpablobr && git pull }
 
 # Display vendor dir tree structure after submodule initialization
 puts `ls -la vendor`
