cb54a9fb5d876e123cb4c6981941e73c81e165de
jpablobr <xjpablobrx@gmail.com>
2010-08-29 21:53:15 -0600

    removing wanderlust dir since it is install at OS level

--- install.rb
+++ install.rb
@@ -1,9 +1,13 @@
 # Author:: Jose Pablo Barrantes (xjpablobrx@gmail.com)
-# Created:: 2010-08-29
-#
-# Submodules Initialization
+# 
+# - Submodules Initialization
+# 
 
 system %Q{ git submodule update --init }
+
+# Fake submoduling yasnippets-jpablobr
 system %Q{ cd vendor && git clone git@github.com:jpablobr/yasnippets-jpablobr.git yasnippets-jpablobr }
 system %Q{ git add yasnippets-jpablobr/ && cd yasnippets-jpablobr && git pull }
+
+# Display vendor dir tree structure after submodule initialization
 puts `ls -la vendor`
