6d37dc993eddc227074cc3ef801b47fa30ef3d9a
jpablobr <xjpablobrx@gmail.com>
2010-09-21 22:37:32 +1000

    adding pymacs

--- jpablobr/python.el
+++ jpablobr/python.el
@@ -1,12 +1,6 @@
 ;;; ----------------------------------------------------------------------------
 ;;; - Python 
 ;;;
-
-(setq auto-mode-alist (cons '("\\.py$" . python-mode) auto-mode-alist))
-(setq interpreter-mode-alist (cons '("python" . python-mode)
-                                     interpreter-mode-alist))
-(autoload 'python-mode "python-mode" "Python editing mode." t)
-
 (setq pylookup-dir "~/.emacs.d/vendor/pylookup")
 (add-to-list 'load-path pylookup-dir)
 ;; load pylookup when compile time
@@ -23,5 +17,57 @@
   "Run pylookup-update and create the database at `pylookup-db-file'." t)
 
 (global-set-key "\C-ch" 'pylookup-lookup)
+
+(autoload 'python-mode "python-mode" "Python Mode." t)
+(add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
+(add-to-list 'interpreter-mode-alist '("python" . python-mode))
+(setq interpreter-mode-alist
+      (cons '("python" . python-mode)
+	    interpreter-mode-alist)
+      python-mode-hook
+      '(lambda () (progn
+		    (set-variable 'py-indent-offset 4)
+		    (set-variable 'py-smart-indentation nil)
+		    (set-variable 'indent-tabs-mode nil) 
+		    ;;(highlight-beyond-fill-column)
+                    (define-key python-mode-map "\C-m" 'newline-and-indent)
+		    (pabbrev-mode)
+		    (abbrev-mode)
+	 )
+      )
+)
+
+;; pymacs
+(autoload 'pymacs-apply "pymacs")
+(autoload 'pymacs-call "pymacs")
+(autoload 'pymacs-eval "pymacs" nil t)
+(autoload 'pymacs-exec "pymacs" nil t)
+(autoload 'pymacs-load "pymacs" nil t)
+(eval-after-load "pymacs" '(add-to-list 'pymacs-load-path "~/.emacs.d/vendor/pymacs"))
+(setq ropemacs-enable-autoimport t)
+
+;; (brm-init)
+;(require 'pycomplete)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Put the following in your .emacs so that the
+;; abbrev table is set correctly in all modes.
+;; (Not just for java)
+;;
+;; (add-hook 'pre-abbrev-expand-hook 'abbrev-table-change)
+;; (defun abbrev-table-change (&optional args)
+;;   (setq local-abbrev-table
+;; 	(if (eq major-mode 'jde-mode)
+;; 	    (if (jde-parse-comment-or-quoted-p)
+;; 		text-mode-abbrev-table
+;; 	      java-mode-abbrev-table)
+;; 	  (if (eq major-mode 'python-mode)
+;; 	      (if (inside-comment-p)
+;; 		  text-mode-abbrev-table
+;; 		python-mode-abbrev-table
+;; 		))))
+;;   )
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (provide 'python)
