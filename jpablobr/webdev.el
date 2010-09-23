0fae96ef289890adde282b9d88b8e7de8324ca53
jpablobr <xjpablobrx@gmail.com>
2010-09-23 11:29:14 +1000

    adding nxhtml mode again

--- jpablobr/webdev.el
+++ jpablobr/webdev.el
@@ -4,7 +4,42 @@
 ;;;
 
 ;;; ---------------------------------------------------------
-;;; - CSS
+;;; - nXML
+;;;
+(load "~/.emacs.d/vendor/nxhtml/autostart.el")
+
+(dolist (i '("xml" "xsd" "rng" "xsl" "xslt" "svg" "rss" "html"))
+  (add-to-list 'auto-mode-alist (cons (concat "\\." i "\\'") 'nxml-mode)))
+
+;;; ---------------------------------------------------------
+;;; - HTML, JavaScript, CSS Setup, YAML
+;;;
+
+(autoload 'php-find-function-prototype "php-functions" "PHP functions" t)
+(autoload 'css-mode          "css-mode" "Mode for editing CSS files" t)
+
+(setq auto-mode-alist  (cons '("\\.css$" . css-mode) auto-mode-alist))
+(setq auto-mode-alist  (cons '("\\.js$" .  js2-mode) auto-mode-alist))
+
+
+(add-hook 'html-mode-hook
+          '(lambda()
+            (local-set-key (kbd "<return>") 'newline-and-indent)))
+
+(add-hook 'css-mode-hook
+          '(lambda()
+            (setq css-indent-offset 2)
+            (setq css-electric-brace-behavior t)
+            (setq css-electric-semi-behavior t)))
+
+;;; ---------------------------------------------------------
+;;; - HTML/mumamo setup
+;;;
+(add-to-list 'load-path "~/.emacs.d/vendor/nxhtml/util")
+(autoload 'rng-clear-overlays "rng-valid" nil t)
+
+;;; ---------------------------------------------------------
+;;; - CSSp
 ;;;
 (add-to-list 'load-path (concat vendor-dir "/css-mode"))
 (require 'css-mode)
