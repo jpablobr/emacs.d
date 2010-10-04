571a71d74baf8532d03bfc069cfb6716d06e69b6
jpablobr <xjpablobrx@gmail.com>
2010-10-04 23:43:14 +1000

    adding haskell-mode

---
+++ jpablobr/jp-haskell.el
@@ -0,0 +1,7 @@
+;;; ----------------------------------------------------------------------------
+;;; - Haskell
+;;; - 
+;;;
+
+(load "~/.emacs.d/vendor/haskell-mode/haskell-site-file")
+(provide 'jp-haskell)
