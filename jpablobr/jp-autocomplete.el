9a6f46abd635ef232382b6cc6fe736b5b11981cf
jpablobr <xjpablobrx@gmail.com>
2010-10-13 03:09:57 +1000

    yasnippets-jpablobr updates for scheme-mode and clojure-mode

--- jpablobr/jp-autocomplete.el
+++ jpablobr/jp-autocomplete.el
@@ -35,7 +35,7 @@
                    c-mode cc-mode c++-mode java-mode
                    perl-mode cperl-mode python-mode ruby-mode jde-mode
                    ecmascript-mode javascript-mode js2-mode espresso-mode php-mode css-mode
-                   makefile-mode sh-mode fortran-mode f90-mode ada-mode inf-ruby-mode
+                   makefile-mode sh-mode fortran-mode f90-mode ada-mode inf-ruby-mode clojure-mode scheme-mode
                    xml-mode sgml-mode sass-mode haml-mode html-mode RoR-mode rinari-mode shell-mode rhtml-mode)))
    ;(add-to-list 'ac-trigger-commands 'org-self-insert-command)
 
