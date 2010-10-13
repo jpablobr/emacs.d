e05027ce4e3f650e12dfe40a139229d8a2014bdc
jpablobr <xjpablobrx@gmail.com>
2010-10-13 18:33:52 +1000

    yaml mode update for .yaml files

--- jpablobr.el
+++ jpablobr.el
@@ -77,3 +77,4 @@
 (add-to-list 'load-path (concat vendor-dir "/yaml-mode"))
 (require 'yaml-mode)
 (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+(add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode))
