f07a57e74d02051c104e38c06d1603dd98f4d799
jpablobr <xjpablobrx@gmail.com>
2010-12-12 05:15:44 +1000

    jde update on custom.el for windows

--- custom.el
+++ custom.el
@@ -1,27 +1,37 @@
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+
  '(default-frame-alist (quote ((tool-bar-lines . 0) (foreground-color\. "white") (background-color . "black") (menu-bar-lines . 1))))
  '(ecb-options-version "2.40")
  '(ecb-source-path (quote ("~/code/")))
  '(erc-modules (quote (autojoin button completion fill irccontrols match menu netsplit noncommands readonly ring scrolltobottom stamp track)))
- '(jde-jdk-registry (quote (("1.5" . "/usr/lib/jvm/java-6-sun"))))
- '(quack-default-program "mit-scheme"))
+ '(quack-default-program "mit-scheme")
  '(jde-bug-debugger-host-address "127.0.0.1")
- '(jde-bug-jre-home "/usr/lib/jvm/java-6-sun")
- '(jde-compile-option-debug (quote ("all" (t nil nil))))
+ '(jde-compile-option-debug (quote ("all" (t nil nil)))) 
  '(jde-debugger (quote ("jdb")))
- '(jde-global-classpath (quote ("." "/usr/share/java/" "/usr/lib/jvm/java-6-sun/")))
- '(jde-jdk-doc-url "/usr/share/doc/sun-java6-jdk/html/api/index.html")
- '(jde-regexp-jar-file "/usr/share/java/regexp.jar")
  '(jde-sourcepath (quote (".")))
 
+(if (eq system-type 'windows-nt)
+    (progn
+      '(jde-jdk-registry (quote (("1.5" . "C:\j2sdk1.6.0_16"))))
+      '(jde-bug-jre-home "C:\j2sdk1.6.0_16")
+      '(jde-global-classpath (quote ("." "/usr/share/java/" "C:\j2sdk1.6.0_16")))
+      '(jde-sourcepath (quote ("."))))
+
+  (progn
+    '(jde-jdk-registry (quote (("1.5" . "/usr/lib/jvm/java-6-sun"))))
+    '(jde-bug-jre-home "/usr/lib/jvm/java-6-sun")
+    '(jde-global-classpath (quote ("." "/usr/share/java/" "/usr/lib/jvm/java-6-sun/")))
+    '(jde-jdk-doc-url "/usr/share/doc/sun-java6-jdk/html/api/index.html")
+    '(jde-regexp-jar-file "/usr/share/java/regexp.jar"))))
+
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(default ((t (:stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 95 :width normal))))
  '(autoface-default ((t (:inherit default :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 95 :width normal)))))
