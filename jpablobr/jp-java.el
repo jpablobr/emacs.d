ff39ac9cdcf05f85a62cd04a4baa02ac2d7153e8
jpablobr <xjpablobrx@gmail.com>
2010-12-14 00:17:21 +1000

    jp-java.el indentation test

--- jpablobr/jp-java.el
+++ jpablobr/jp-java.el
@@ -12,46 +12,46 @@
 
 (setq jde-auto-parse-enable nil)
 (setq jde-enable-senator nil)
-(load "jde-autoload")
+
+(add-hook 'jde-mode-hook
+          '(lambda ()
+     (load "jde-autoload")))
+
 
 ;; If you want Emacs to defer loading the JDE until you open a 
 ;; Java file, edit the following line
 (setq defer-loading-jde t)
-;; to read:
-;;
-;;  (setq defer-loading-jde t)
-;;
-
-(defun skip-cleanup())
 
 ;; function does not exist in emacs 23.2
 (defun semantic-parse())
+(defun skip-cleanup())
+(defun  semantic-format-prototype-tag-java-mode())
  
 ;; Sets the basic indentation for Java source files
 ;; to two spaces.
-(defun my-jde-mode-hook ()
-  "Hook for running java file..."
-  (message " Loading my-jde-mode-hook...")
-  (c-set-offset 'substatement-open 0)
-  (c-set-offset 'statement-case-open 0)
-  (c-set-offset 'case-label '+)
- (wisent-java-default-setup)
-  (setq 
-   indent-tabs-mode nil
-   tab-width 2
-   c-basic-offset 2
-   tempo-interactive t
-   ))
+;; (defun my-jde-mode-hook ()
+;;   "Hook for running java file..."
+;;   (message " Loading my-jde-mode-hook...")
+;;   (c-set-offset 'substatement-open 0)
+;;   (c-set-offset 'statement-case-open 0)
+;;   (c-set-offset 'case-label '+)
+;;  (wisent-java-default-setup)
+;;   (setq 
+;;    indent-tabs-mode nil
+;;    tab-width 2
+;;    c-basic-offset 2
+;;    tempo-interactive t
+;;    ))
 
 (add-hook 'jde-mode-hook 'my-jde-mode-hook)
 
 ;;; ----------------------------------------------------------------------------
 ;;; - ECB 
 ;;;
-(add-hook 'jde-mode-hook
-          '(lambda ()
-     (require 'ecb)
-     (ecb-minor-mode 1)))
+;; (add-hook 'jde-mode-hook
+;;           '(lambda ()
+;;      (require 'ecb)
+;;      (ecb-minor-mode 1)))
 
 (require 'jde)
 (provide 'jp-java)
