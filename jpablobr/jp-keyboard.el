aacc84608ef16d050000ae258b026e720d63aadb
jpablobr <xjpablobrx@gmail.com>
2011-01-06 03:55:10 +1000

    goto-line key binding

--- jpablobr/jp-keyboard.el
+++ jpablobr/jp-keyboard.el
@@ -164,6 +164,7 @@
 (global-set-key [(backtab)] 'textmate-shift-left)
 (global-set-key [(meta /)] 'comment-or-uncomment-region-or-line)
 (global-set-key [(meta L)] 'textmate-select-line)
+(global-set-key [(meta l)] 'goto-line)
 
 ;;; Jump to matching parent
 (global-set-key "%" 'match-paren)
