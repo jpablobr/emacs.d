c26db02ab5789d28147de6164e6fc585e6694d42
jpablobr <xjpablobrx@gmail.com>
2011-02-01 00:42:08 +1000

    commented out word-count

--- jpablobr/jp-misc.el
+++ jpablobr/jp-misc.el
@@ -351,36 +351,37 @@ LIST defaults to all existing live buffers."
 
 (display-time-mode t)
 
-(defvar count-words-buffer
-  nil
-  "*Number of words in the buffer.")
+;; Word count stuff
+;; (defvar count-words-buffer
+;;   nil
+;;   "*Number of words in the buffer.")
 
-(defun wicked/update-wc ()
-  (interactive)
-  (setq count-words-buffer (number-to-string (count-words-buffer)))
-  (force-mode-line-update))
+;; (defun wicked/update-wc ()
+;;   (interactive)
+;;   (setq count-words-buffer (number-to-string (count-words-buffer)))
+;;   (force-mode-line-update))
 
-; only setup timer once
-(unless count-words-buffer
-  ;; seed count-words-paragraph
-  ;; create timer to keep count-words-paragraph updated
-  (run-with-idle-timer 1 t 'wicked/update-wc))
+;; ; only setup timer once
+;; (unless count-words-buffer
+;;   ;; seed count-words-paragraph
+;;   ;; create timer to keep count-words-paragraph updated
+;;   (run-with-idle-timer 1 t 'wicked/update-wc))
 
-;; add count words paragraph the mode line
-(unless (memq 'count-words-buffer global-mode-string)
-  (add-to-list 'global-mode-string "words: " t)
-  (add-to-list 'global-mode-string 'count-words-buffer t))
+;; ;; add count words paragraph the mode line
+;; (unless (memq 'count-words-buffer global-mode-string)
+;;   (add-to-list 'global-mode-string "words: " t)
+;;   (add-to-list 'global-mode-string 'count-words-buffer t))
 
-;; count number of words in current paragraph
-(defun count-words-buffer ()
-  "Count the number of words in the current paragraph."
-  (interactive)
-  (save-excursion
-    (goto-char (point-min))
-    (let ((count 0))
-      (while (not (eobp))
-	(forward-word 1)
-        (setq count (1+ count)))
-      count)))
+;; ;; count number of words in current paragraph
+;; (defun count-words-buffer ()
+;;   "Count the number of words in the current paragraph."
+;;   (interactive)
+;;   (save-excursion
+;;     (goto-char (point-min))
+;;     (let ((count 0))
+;;       (while (not (eobp))
+;;  (forward-word 1)
+;;         (setq count (1+ count)))
+;;       count)))
 
 (provide 'jp-misc)
