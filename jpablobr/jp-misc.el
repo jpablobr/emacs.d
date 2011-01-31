55b11d1e1dc7e73db5059cb2a7e8992c59dcaeb6
jpablobr <xjpablobrx@gmail.com>
2011-01-31 16:05:15 +1000

    org-mode updates wordcount and display time

--- jpablobr/jp-misc.el
+++ jpablobr/jp-misc.el
@@ -1,5 +1,5 @@
  ;;; -*- coding: utf-8-unix; -*-
-;;; ---------------------------------------------------------
+;;jp-misc.el ---------------------------------------------------------
 ;;; - Things that don't fit anywhere else
 ;;;
 (when window-system
@@ -334,5 +334,53 @@ LIST defaults to all existing live buffers."
 
 (require 'template)
 (template-initialize)
+
+(defface egoge-display-time
+  '((((type x w32 mac))
+     ;; #060525 is the background colour of my default face.
+     (:foreground "green" :inherit bold))
+    (((type tty))
+     (:foreground "green")))
+  "Face used to display the time in the mode line.")
+
+;; This causes the current time in the mode line to be displayed in
+;; `egoge-display-time-face' to make it stand out visually.
+(setq display-time-string-forms
+      '((propertize (concat " " 12-hours ":" minutes " ")
+                    'face 'egoge-display-time)))
+
+(display-time-mode t)
+
+(defvar count-words-buffer
+  nil
+  "*Number of words in the buffer.")
+
+(defun wicked/update-wc ()
+  (interactive)
+  (setq count-words-buffer (number-to-string (count-words-buffer)))
+  (force-mode-line-update))
+
+; only setup timer once
+(unless count-words-buffer
+  ;; seed count-words-paragraph
+  ;; create timer to keep count-words-paragraph updated
+  (run-with-idle-timer 1 t 'wicked/update-wc))
+
+;; add count words paragraph the mode line
+(unless (memq 'count-words-buffer global-mode-string)
+  (add-to-list 'global-mode-string "words: " t)
+  (add-to-list 'global-mode-string 'count-words-buffer t))
+
+;; count number of words in current paragraph
+(defun count-words-buffer ()
+  "Count the number of words in the current paragraph."
+  (interactive)
+  (save-excursion
+    (goto-char (point-min))
+    (let ((count 0))
+      (while (not (eobp))
+	(forward-word 1)
+        (setq count (1+ count)))
+      count)))
 
 (provide 'jp-misc)
--- jpablobr/jp-org-config.el
+++ jpablobr/jp-org-config.el
@@ -1,8 +1,8 @@
 ;;; -*- coding: utf-8-unix; -*-
-;;; ---------------------------------------------------------
+;;jp-org-config.el ---------------------------------------------------------
 ;;; - ORG mode
 ;;;
-;(custom-set-faces '(default ((t (:height 100 )))))
+                                        ;(custom-set-faces '(default ((t (:height 100 )))))
 
 (require 'org-install)
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
@@ -22,10 +22,70 @@
 
 ;; Set the agenda files
 (setq org-agenda-files (list "~/Dropbox/org-mode/work.org"
-                             "~/Dropbox/org-mode/school.org" 
+                             "~/Dropbox/org-mode/school.org"
                              "~/Dropbox/org-mode/jpablobr.org"))
 
 ;; Set to <your Dropbox root directory>/MobileOrg.
 (setq org-mobile-directory "~/Dropbox/MobileOrg")
+
+(defun wicked/org-update-checkbox-count (&optional all)
+  "Update the checkbox statistics in the current section.
+This will find all statistic cookies like [57%] and [6/12] and update
+them with the current numbers.  With optional prefix argument ALL,
+do this for the whole buffer."
+  (interactive "P")
+  (save-excursion
+    (let* ((buffer-invisibility-spec (org-inhibit-invisibility))
+           (beg (condition-case nil
+                    (progn (outline-back-to-heading) (point))
+                  (error (point-min))))
+           (end (move-marker
+                 (make-marker)
+                 (progn (or (outline-get-next-sibling) ;; (1)
+                            (goto-char (point-max)))
+                        (point))))
+           (re "\\(\\[[0-9]*%\\]\\)\\|\\(\\[[0-9]*/[0-9]*\\]\\)")
+           (re-box
+            "^[ \t]*\\(*+\\|[-+*]\\|[0-9]+[.)]\\) +\\(\\[[- X]\\]\\)")
+           b1 e1 f1 c-on c-off lim (cstat 0))
+      (when all
+        (goto-char (point-min))
+        (or (outline-get-next-sibling) (goto-char (point-max))) ;; (2)
+        (setq beg (point) end (point-max)))
+      (goto-char beg)
+      (while (re-search-forward re end t)
+        (setq cstat (1+ cstat)
+              b1 (match-beginning 0)
+              e1 (match-end 0)
+              f1 (match-beginning 1)
+              lim (cond
+                   ((org-on-heading-p)
+                    (or (outline-get-next-sibling) ;; (3)
+                        (goto-char (point-max)))
+                    (point))
+                   ((org-at-item-p) (org-end-of-item) (point))
+                   (t nil))
+              c-on 0 c-off 0)
+        (goto-char e1)
+        (when lim
+          (while (re-search-forward re-box lim t)
+            (if (member (match-string 2) '("[ ]" "[-]"))
+                (setq c-off (1+ c-off))
+              (setq c-on (1+ c-on))))
+          (goto-char b1)
+          (insert (if f1
+                      (format "[%d%%]" (/ (* 100 c-on)
+                                          (max 1 (+ c-on c-off))))
+                    (format "[%d/%d]" c-on (+ c-on c-off))))
+          (and (looking-at "\\[.*?\\]")
+               (replace-match ""))))
+      (when (interactive-p)
+        (message "Checkbox statistics updated %s (%d places)"
+                 (if all "in entire file" "in current outline entry")
+                 cstat)))))
+(defadvice org-update-checkbox-count (around wicked activate)
+  "Fix the built-in checkbox count to understand headlines."
+  (setq ad-return-value
+        (wicked/org-update-checkbox-count (ad-get-arg 1))))
 
 (provide 'jp-org-config)
