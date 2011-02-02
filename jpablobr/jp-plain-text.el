6774a8d87622cf0ed3ad17038afe73e0814d831f
jpablobr <xjpablobrx@gmail.com>
2011-02-02 20:09:08 +1000

    adding sudo-save mode

--- jpablobr/jp-plain-text.el
+++ jpablobr/jp-plain-text.el
@@ -31,71 +31,36 @@
     (goto-char (point-min))
     (while (search-forward "_" nil t) (replace-match " ")) ))
 
-(defun replace-underscore-space-toggle ()
-  "Replace underscore/space in the current region or line.
-If the current line contains more “_” char than space,
-then replace them to space, else replace space to _.
-If there's a text selection, work on the selected text."
+(defvar count-words-buffer
+  nil
+  "*Number of words in the buffer.")
+
+(defun wicked/update-wc ()
   (interactive)
-  (let (li bds)
-    (setq bds
-          (if (region-active-p)
-              (cons (region-beginning) (region-end))
-            (bounds-of-thing-at-point 'line)))
-    (setq li (buffer-substring-no-properties (car bds) (cdr bds)))
-    (if (> (count 32 li) (count 95 li))
-        (progn (replace-string " " "_" nil (car bds) (cdr bds)))
-      (progn (replace-string "_" " " nil (car bds) (cdr bds))))))
+  (setq count-words-buffer (number-to-string (count-words-buffer)))
+  (force-mode-line-update))
 
-(defun mapcar-head (fn-head fn-rest list)
-  "Like MAPCAR, but applies a different function to the first element."
-  (if list
-      (cons (funcall fn-head (car list)) (mapcar fn-rest (cdr list)))))
-(defun camelize (s)
-  "Convert under_score string S to CamelCase string."
-  (mapconcat 'identity (mapcar
-                        '(lambda (word) (capitalize (downcase word)))
-                        (split-string s "_")) ""))
-(defun camelize-method (s)
-  "Convert under_score string S to camelCase string."
-  (mapconcat 'identity (mapcar-head
-                        '(lambda (word) (downcase word))
-                        '(lambda (word) (capitalize (downcase word)))
-                        (split-string s "_")) ""))
-(defun un-camelcase-string (s &optional sep start)
-  "Convert CamelCase string S to lower case with word separator SEP.
-    Default for SEP is a hyphen \"-\".
-    If third argument START is non-nil, convert words after that
-    index in STRING."
-  (let ((case-fold-search nil))
-    (while (string-match "[A-Z]" s (or start 1))
-      (setq s (replace-match (concat (or sep "-")
-                                     (downcase (match-string 0 s)))
-                             t nil s)))
-    (downcase s)))
+; only setup timer once
+(unless count-words-buffer
+  ;; seed count-words-paragraph
+  ;; create timer to keep count-words-paragraph updated
+  (run-with-idle-timer 1 t 'wicked/update-wc))
 
-    (defun split-name (s)
-      (split-string
-       (let ((case-fold-search nil))
-	 (downcase
-	  (replace-regexp-in-string "\\([a-z]\\)\\([A-Z]\\)" "\\1 \\2" s)))
-       "[^A-Za-z0-9]+"))
-    (defun camelcase  (s) (mapconcat 'capitalize (split-name s) ""))
-    (defun underscore (s) (mapconcat 'downcase   (split-name s) "_"))
-    (defun dasherize  (s) (mapconcat 'downcase   (split-name s) "-"))
-    (defun colonize   (s) (mapconcat 'capitalize (split-name s) "::"))
-    (defun camelscore (s)
-      (cond ((string-match-p "\\(?:[a-z]+_\\)+[a-z]+" s)	(dasherize  s))
-	    ((string-match-p "\\(?:[a-z]+-\\)+[a-z]+" s)	(camelcase  s))
-	    ((string-match-p "\\(?:[A-Z][a-z]+\\)+$"  s)	(colonize   s))
-	    (t						(underscore s)) ))
-    (defun camelscore-word-at-point ()
-      (interactive)
-      (let* ((case-fold-search nil)
-	     (beg (and (skip-chars-backward "[:alnum:]:_-") (point)))
-	     (end (and (skip-chars-forward  "[:alnum:]:_-") (point)))
-	     (txt (buffer-substring beg end))
-	     (cml (camelscore txt)) )
-	(if cml (progn (delete-region beg end) (insert cml))) ))
+;; add count words paragraph the mode line
+(unless (memq 'count-words-buffer global-mode-string)
+  (add-to-list 'global-mode-string "words: " t)
+  (add-to-list 'global-mode-string 'count-words-buffer t))
+
+;; count number of words in current paragraph
+(defun count-words-buffer ()
+  "Count the number of words in the current paragraph."
+  (interactive)
+  (save-excursion
+    (goto-char (point-min))
+    (let ((count 0))
+      (while (not (eobp))
+ (forward-word 1)
+        (setq count (1+ count)))
+      count)))
 
 (provide 'jp-plain-text)
