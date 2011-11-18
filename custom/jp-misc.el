4860dd0fe2365e442777dabdc2fc07989e91df70
jpablobr <xjpablobrx@gmail.com>
2011-11-19 01:50:47 +1000

    removing dictionary

--- custom/jp-misc.el
+++ custom/jp-misc.el
@@ -102,26 +102,6 @@
 ;; - Find-recursive
  (require 'find-recursive)
 
-;; --------------------------------------------------------------------
-;; - Dictionary
-;; - % sudo apt-get install dictionary-el
-(autoload 'dictionary-search "dictionary"
-  "Ask for a word and search it in all dictionaries" t)
-(autoload 'dictionary-match-words "dictionary"
-  "Ask for a word and search all matching words in the dictionaries" t)
-(autoload 'dictionary-lookup-definition "dictionary"
-  "Unconditionally lookup the word at point." t)
-(autoload 'dictionary "dictionary"
-  "Create a new dictionary buffer" t)
-(autoload 'dictionary-mouse-popup-matching-words "dictionary"
-  "Display entries matching the word at the cursor" t)
-(autoload 'dictionary-popup-matching-words "dictionary"
-  "Display entries matching the word at the point" t)
-(autoload 'dictionary-tooltip-mode "dictionary"
-  "Display tooltips for the current word" t)
-(autoload 'global-dictionary-tooltip-mode "dictionary"
-  "Enable/disable dictionary-tooltip-mode for all buffers" t)
-
 ;; ---------------------------------------------------------------------
 ;; - Display time
 (defface egoge-display-time
@@ -222,6 +202,9 @@
 
 (setq uniquify-buffer-name-style 'forward)
 (require 'uniquify)
+
+(require 'auto-dictionary)
+(add-hook 'flyspell-mode-hook (lambda () (auto-dictionary-mode 1)))
 
 (custom-set-faces '(mode-line ((t (:foreground "#94bff3" :background "#222222"))))
                   '(font-lock-comment-face ((t (:foreground "#7f9f7f"))))
