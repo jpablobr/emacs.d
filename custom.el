8bf3674b44f09b7fc639d19979b1dd67e93c0f0b
jpablobr <xjpablobrx@gmail.com>
2012-02-06 10:34:17 +1000

    md: anything mode color minor changes

--- custom.el
+++ custom.el
@@ -13,6 +13,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(anything-ff-directory ((t (:background "brightblack" :foreground "color-28"))))
+ '(anything-ff-file ((t (:foreground "green" :underline t))))
  '(erb-comment-delim-face ((t (:background "brightblack" :foreground "red" :weight thin))))
  '(erb-comment-face ((t (:background "brightblack" :foreground "red" :weight thin))))
  '(erb-delim-face ((t (:background "brightblack" :foreground "blue" :weigth thin))))
